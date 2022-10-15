df4ff18ba199ed68507f19417ca6b9f379d24e65
Gary_Zhang <zyf3037900395@gmail.com>
2022-10-15 21:15:04 +0800

    完成信用卡图像处理

--- shizhan1/ocr_template_match.py
+++ shizhan1/ocr_template_match.py
@@ -18,6 +18,16 @@ def cv_show(img, name):
     cv2.destroyAllWindows()
 
 
+def resize(img, width):
+    # 等比例改变图像大小
+    w, h = img.shape[0], img.shape[1]
+    new_w = width
+    new_h = int(width * h / w)  # 必须是整数
+    print(new_w, new_h)
+    new_img = cv2.resize(img, (new_h, new_w))
+    return new_img
+
+
 img = cv2.imread('images/numbers.png')
 cv_show(img, 'img')
 ref = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -61,3 +71,48 @@ for (i, c) in enumerate(refCnts):
     digits[i] = roi
 
 # 初始化卷积核
+rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
+sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
+# 灰度处理，改变大小
+image = cv2.imread('images/credit_card_01.png')
+cv_show(image, 'image')
+image = resize(image, width=200)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv_show(gray, 'gray')
+
+# 礼帽操作，突出明亮的区域
+tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+cv_show(tophat, 'tophat')
+
+sobelx = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=-1)
+# 负数是黑色的
+sobelx = cv2.convertScaleAbs(sobelx)  # 绝对值转换
+
+sobely = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=3)
+sobely = cv2.convertScaleAbs(sobely)  # 绝对值转换
+
+# 分别计算x和y，再求和
+# addweighted求和
+sobelxy = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
+# cv_show(sobelxy, "sobelxy")
+
+# 归一化一下
+sobelxy = np.absolute(sobelxy)
+(minVal, maxVal) = (np.min(sobelxy), np.max(sobelxy))
+sobelxy = (255 * ((sobelxy - minVal) / (maxVal - minVal)))
+sobelxy = sobelxy.astype('uint8')
+cv_show(sobelxy, "sobelxy")
+
+# 闭操作，让数字连在一起
+sobelxy = cv2.morphologyEx(sobelxy, cv2.MORPH_CLOSE, rectKernel)
+cv_show(sobelxy, "sobelxy")
+
+# 再二值化一下 THRESH_OTSU自动寻找合适的阈值
+thresh = cv2.threshold(sobelxy, 0, 255,
+                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+cv_show(thresh, "thresh")
+# 再开操作一下，把细节处理掉，再开操作把空填上
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, rectKernel)
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, rectKernel)
+cv_show(thresh, "thresh")
